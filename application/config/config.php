594303b9fa23bf454f7a7c7a48d2a8ad228f09d3
Ana Paula Scariot <anapaula.scariot@hotmail.com>
2019-10-09 12:29:39 -0500

    camino ok

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/chiquillas';
+$config['base_url'] = 'http://localhost/APIrevo_rest';
 
 /*
 |--------------------------------------------------------------------------
