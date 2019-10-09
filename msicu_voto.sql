cea5b09f25ad345a82c465faf778f849e49e4817
QscarH <44385774+QscarH@users.noreply.github.com>
2019-10-09 13:13:35 -0500

    Base de datos
    La base de datos

--- msicu_voto.sql
+++ msicu_voto.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.4.12
--- http://www.phpmyadmin.net
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 09-10-2019 a las 19:05:21
--- Versión del servidor: 5.6.25
--- Versión de PHP: 5.6.11
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 09-10-2019 a las 18:12:35
+-- Versión del servidor: 5.7.26
+-- Versión de PHP: 7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -26,11 +28,22 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `respuesta`
 --
 
+DROP TABLE IF EXISTS `respuesta`;
 CREATE TABLE IF NOT EXISTS `respuesta` (
-  `id_respuesta` int(10) NOT NULL,
-  `id_usuario` int(10) NOT NULL,
-  `voto` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `idpregunta` int(11) NOT NULL AUTO_INCREMENT,
+  `pregunta` varchar(30) COLLATE utf8mb4_bin NOT NULL,
+  `voto` int(11) NOT NULL,
+  PRIMARY KEY (`idpregunta`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+--
+-- Volcado de datos para la tabla `respuesta`
+--
+
+INSERT INTO `respuesta` (`idpregunta`, `pregunta`, `voto`) VALUES
+(1, 'Bueno', 2),
+(2, 'Regular', 0),
+(3, 'Malo', 1);
 
 -- --------------------------------------------------------
 
@@ -38,47 +51,25 @@ CREATE TABLE IF NOT EXISTS `respuesta` (
 -- Estructura de tabla para la tabla `usuario`
 --
 
+DROP TABLE IF EXISTS `usuario`;
 CREATE TABLE IF NOT EXISTS `usuario` (
   `id_usuario` int(10) NOT NULL,
   `email` varchar(100) DEFAULT NULL,
   `contrasena` varchar(100) DEFAULT NULL,
   `nombre` varchar(100) DEFAULT NULL,
-  `apellido` varchar(100) DEFAULT NULL
+  `apellido` varchar(100) DEFAULT NULL,
+  `voto` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_usuario`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `usuario`
 --
 
-INSERT INTO `usuario` (`id_usuario`, `email`, `contrasena`, `nombre`, `apellido`) VALUES
-(1, 'alicia16yazmin@gmail.com', '123456', 'ali', 'luna'),
-(2, 'oscar1@gmail.com', '654321', 'oscar', 'hdz');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `respuesta`
---
-ALTER TABLE `respuesta`
-  ADD KEY `id_usuario` (`id_usuario`);
-
---
--- Indices de la tabla `usuario`
---
-ALTER TABLE `usuario`
-  ADD PRIMARY KEY (`id_usuario`);
-
---
--- Restricciones para tablas volcadas
---
-
---
--- Filtros para la tabla `respuesta`
---
-ALTER TABLE `respuesta`
-  ADD CONSTRAINT `respuesta_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuario` (`id_usuario`);
+INSERT INTO `usuario` (`id_usuario`, `email`, `contrasena`, `nombre`, `apellido`, `voto`) VALUES
+(1, 'alicia16yazmin@gmail.com', '123456', 'ali', 'luna', 0),
+(2, 'oscar1@gmail.com', '654321', 'oscar', 'hdz', 0);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
